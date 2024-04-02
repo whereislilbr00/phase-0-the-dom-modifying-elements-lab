b997bca7e53b75d5c7c4f5a33d5aa08ee22081c2
louis <louis.lamonte@moringaschool.com>
2024-04-02 15:41:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let element = document .getElementById("main")
+element.remove(main)
+let newHeader = document.createElement("h1")
+newHeader.id ="victory"
+newHeader . innerHTML = "louis is the champion"
